4eadb48caa06e40559120e1b5a7943755c09aa68
Softwarica College Lab <softwarica@softwarica.com>
2019-12-26 16:54:14 +0545

    Dashboard Card view and recycler view

--- app/src/main/java/com/example/darazz/MainActivity.java
+++ app/src/main/java/com/example/darazz/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.darazz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView info_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        info_text = findViewById(R.id.info_text);
     }
 }
