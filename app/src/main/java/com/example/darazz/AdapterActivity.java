4646c0654cfc16c94f56ffe0d22af2a1c5f4732d
Softwarica College Lab <softwarica@softwarica.com>
2019-12-30 16:32:15 +0545

    Navigation

--- app/src/main/java/com/example/darazz/AdapterActivity.java
+++ app/src/main/java/com/example/darazz/AdapterActivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -16,13 +18,44 @@ import java.util.List;
 public class AdapterActivity extends RecyclerView.Adapter<AdapterActivity.MyHolder> {
     List<Item> item_list = new ArrayList<>();
     Context contexts;
-}
-public AdapterActivity(List<Item> item_list, Context contexts){
-    this.item_list = item_list;
-    this.contexts = contexts;
-}
-@NonNull
-@Override
-public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
-    View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.)
+
+    public AdapterActivity(List<Item> item_list, Context contexts) {
+        this.item_list = item_list;
+        this.contexts = contexts;
+    }
+
+    @NonNull
+    @Override
+    public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout, parent, false);
+        MyHolder myHolder = new MyHolder(view);
+        return myHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyHolder holder, final int position) {
+        final Item item = item_list.get(position);
+        holder.itemname.setText(item.getName());
+        holder.qty.setText(item.getQty());
+        holder.price.setText(item.getPrice());
+        holder.year.setText(item.getYear());
+        holder.imageview.setImageDrawable(contexts.getResources().getDrawable(R.drawable.ic_delete));
+    }
+
+    @Override
+    public int getItemCount(){
+        return item_list.size();
+    }
+    public class MyHolder extends RecyclerView.ViewHolder{
+        ImageView imageview, delete;
+        TextView itemname, qty, price, year;
+        public  MyHolder(@NonNull View itemView){
+            super(itemView);
+            imageview = itemView.findViewById(R.id.image);
+            itemname = itemView.findViewById(R.id.itemname);
+            qty = itemView.findViewById(R.id.itemqty);
+            price = itemView.findViewById(R.id.itemprice);
+            year = itemView.findViewById(R.id.itemManufac);
+        }
+    }
 }
--- app/src/main/java/com/example/darazz/MainActivity.java
+++ app/src/main/java/com/example/darazz/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.darazz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.widget.TextView;
@@ -20,5 +24,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         BottomNavigationView navigationView = findViewById(R.id.nav_view);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.navigation_home,R.id.navigation_add_item,R.id.navigation_account).build();
+
+        NavController navController = Navigation.findNavController(this,R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this,navController,appBarConfiguration);
+
+
+        NavigationUI.setupActionBarWithNavController(navigationView,navController);
     }
 }
